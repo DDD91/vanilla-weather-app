e3ad28d14e308e7a0e9d80c44c95f41c4c8447ca
DDD91 <ghecci1614@gmail.com>
2021-12-23 21:35:01 -0500

    Updates to the index file
    added spans to humidity & wind
    added ids to the h1,  humidity & wind

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
-let apiKey = "c719a2f88c471ef6e9dcae6126323c39";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
-console.log(apiUrl);
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTMl = response.data.main.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "b15398b81e4c7d02bbf4a9feebcdde90";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+q=New York&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayTemperature);
