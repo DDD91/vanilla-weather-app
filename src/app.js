863b035bab28c607d9649ad727ebb12283e2cd93
DDD91 <ghecci1614@gmail.com>
2021-12-24 23:30:08 -0500

    Search bar
    Added the form for the search bar

--- src/app.js
+++ src/app.js
@@ -15,25 +15,28 @@ return `${day} ${hours}:${minutes}`;
 
 
 function displayTemperature(response) {
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let iconElement = document.querySelector("#icon");
+;    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTMl = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = "b15398b81e4c7d02bbf4a9feebcdde90";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=New York&appid=${apiKey}&units=metric`;
+q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
